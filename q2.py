00f14d5f7356f6328931b35893816431374642d8
dharmeshbharadiya <bca2022dharmesh1738@tnraocollege.org>
2024-08-12 19:54:56 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,16 @@
+emp = {
+    'id': 1,
+    'name': 'rohan',
+    'designation': 'Software Engineer',
+    'salary': 50000
+}
+
+print("Original Dictionary:")
+print(emp)
+print("delete designation:")
+del emp['designation']
+print(emp)
+print("update name:
+")
+emp['name'] = 'karan'
+print(emp)
